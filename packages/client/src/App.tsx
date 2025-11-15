688902d48080c0debc067e81881d58f529fe9a41
Reda Hilal <reda0x04@gmail.com>
2025-11-15 10:13:34 +0400

    add theme provider

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -13,11 +15,12 @@ function App() {
   }, []);
 
   return (
-    <>
+    <ThemeProvider>
       <p>{message}</p>
       <p className="font-bold p-4 text-2xl">Hello World!</p>
       <Button>Click me</Button>
-    </>
+      <ModeToggle />
+    </ThemeProvider>
   );
 }
 
