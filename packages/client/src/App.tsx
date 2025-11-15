b57f63bc041e6cc5696043f51d4456c040608ff4
Reda Hilal <reda0x04@gmail.com>
2025-11-15 10:04:11 +0400

    install tailwind and shadcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -11,7 +12,13 @@ function App() {
       .catch((error) => console.error("Error fetching message:", error));
   }, []);
 
-  return <p>{message}</p>;
+  return (
+    <>
+      <p>{message}</p>
+      <p className="font-bold p-4 text-2xl">Hello World!</p>
+      <Button>Click me</Button>
+    </>
+  );
 }
 
 export default App;
