660c62ecf20beb63f87e50145b0ec83a984c621a
Reda Hilal <reda0x04@gmail.com>
2025-11-19 13:47:46 +0400

    building the chat api

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 dotenv.config();
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json()); // Middleware to parse JSON bodies
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
@@ -12,7 +19,21 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World!" });
+  res.json({ message: "This is a Json response" });
+});
+
+// test using postman
+app.post("/api/chat", async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+
+  const response = await openai.responses.create({
+    model: "gpt-4.1-nano",
+    input: prompt,
+    temperature: 0.3,
+    max_output_tokens: 200,
+  });
+
+  res.json({ reply: response.output_text });
 });
 
 app.listen(PORT, () => {
